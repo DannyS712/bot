466a539e82cfb82255228d8490722d636fc6c7e5
DannyS712 <46829944+DannyS712@users.noreply.github.com>
2020-03-22 19:11:53 -0700

    Start
    See https://en.wikipedia.org/wiki/Wikipedia:Bots/Requests_for_approval/DannyS712_bot_69

---
+++ DRN cleaner.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+// Dependencies.
+const argv = require('minimist')(process.argv.slice(2));
+const MWBot = require('mwbot');
+const mysql = require('mysql');
+const util = require('util');
+
+const credentials = require('./credentials'); // Load credentials from config.
+const apiUrl = 'https://en.wikipedia.org/w/api.php';
+const database = 'enwiki_p';
+const noticeboard = 'Wikipedia:Dispute resolution noticeboard';
+const editSummary = 'Remove do not archive tags from closed cases - BOT in trial - [[Wikipedia:Bots/Requests for approval/DannyS712 bot 69|BRFA]]';
+
+/**
+ * Log a message to stdout prepended with a timestamp.
+ * @param {String} message
+ */
+function log(message) {
+    const datestamp = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    console.log(`${datestamp}: ${message}`);
+}
+
+/**
+ * Update the noticeboard
+ * @param {bool} dryRun
+ * @returns {Promise<void>}
+ */
+async function cleanupNoticeboard(dryRun) {
+    // Login to the bot.
+    log(`Logging in to bot account`);
+    const bot = new MWBot({apiUrl});
+    await bot.loginGetEditToken({
+        apiUrl,
+        username: credentials.username,
+        password: credentials.password
+    });
+
+    // Get the current content
+    const queryResult = await bot.read( noticeboard );
+    const content = queryResult.query.pages[ pageID ].revisions[ 0 ][ '*' ];
+    const newContent = content.replace( /({{DR case status\|(?:reject|resolve(?:d)?|fail(?:ed)?|close(?:d)?)}})\n<!-- \[\[User:DoNotArchiveUntil.*?-->{{User:ClueBot III\/DoNotArchiveUntil\|\d+}}<!--.*?-->/g, "$1" );
+
+    // Edit the page.
+    if ( dryRun ) {
+        log( content, newContent );
+        return;
+    } else {
+        return await bot.update( noticeboard, newContent, editSummary, { minor: true } );
+    }
+}
+
+/**
+ * Entry point for the bot task.
+ * @returns {Promise<void>}
+ */
+async function main() {
+    await cleanupNoticeboard(argv.dry);
+    log('Task complete!');
+    process.exit();
+}
+
+main().catch(console.error);
