1500718f9dbd5588e6e41047b4004857ca3fa647
DannyS712 <46829944+DannyS712@users.noreply.github.com>
2024-06-12 07:39:21 -0700

    stub-name: don't add extra whitespace

--- stub-name.js
+++ stub-name.js
@@ -71,7 +71,7 @@ async function fixStubTemplate( bot, row, dryRun ) {
   const content = queryResult.query.pages[ pageID ].revisions[ 0 ][ '*' ];
   // replace until whitespace OR the } at the end of the template if
   // all on one line
-  const newContent = content.replace( /(\|\s*name\s*=\s*)\S*?($|\s|})/i, "$1 {{subst:FULLPAGENAME}}$2" );
+  const newContent = content.replace( /(\|\s*name\s*=\s*)\S*?($|\s|})/i, "$1{{subst:FULLPAGENAME}}$2" );
   if ( dryRun ) {
     console.log( title, content, newContent );
     return;
